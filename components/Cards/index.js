6950eae5ad97f7bf8fd31145c2831b430df75896
Cristina Altreche <c.altreche@gmail.com>
2020-06-05 13:38:17 -0500

    cards showing

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -20,9 +20,7 @@
 // Use your function to create a card for each of the articles and add the card to the DOM.
 let cardsContainer = document.querySelector(".cards-container");
 
-function cardMaker(elements) {
-
-  const {head, photo, auth} = ;     
+function cardMaker(head, photo, auth) {
   let card = document.createElement("div");
   let headline = document.createElement("div");
   let author = document.createElement("div");
@@ -54,22 +52,27 @@ console.log(cardMaker());
 axios
   .get("https://lambda-times-backend.herokuapp.com/articles")
   .then((response) => {
-    // console.log(response.data);
-    let articleArr = response.data.articles;
-    console.log(articleArr);
+    console.log(response.data);
+
     let combinedArticles = [];
+    combinedArticles.push(response.data.articles.bootstrap[0]);
+    combinedArticles.push(response.data.articles.bootstrap[1]);
+    combinedArticles.push(response.data.articles.bootstrap[2]);
+    combinedArticles.push(response.data.articles.javascript[0]);
+    combinedArticles.push(response.data.articles.javascript[1]);
+    combinedArticles.push(response.data.articles.javascript[2]);
+    combinedArticles.push(response.data.articles.javascript[3]);
+    combinedArticles.push(response.data.articles.jquery[0]);
+    combinedArticles.push(response.data.articles.jquery[1]);
+    combinedArticles.push(response.data.articles.jquery[2]);
+    combinedArticles.push(response.data.articles.node[0]);
+    combinedArticles.push(response.data.articles.node[1]);
+    combinedArticles.push(response.data.articles.technology[0]);
+    combinedArticles.push(response.data.articles.technology[1]);
+    combinedArticles.push(response.data.articles.technology[2]);
+    console.log(combinedArticles);
 
-    // combinedArticles = articleArr.forEach((item) => item.map((m) => m.headline
-
-    // }))
-
-    for (let i = 0; i < articleArr.length; i++) {
-      let headline = articleArr[i].map((m) => m.headline);
-      let photo = articleArr[i].map((m) => m.authorPhoto);
-      let name = articleArr[i].map((m) => m.authorName);
-      combinedArticles.push(headline, photo, name);
-      console.log(combinedArticles);
-    }
+ 
 
     combinedArticles.forEach((element) => {
       const newCard = cardMaker(element);
